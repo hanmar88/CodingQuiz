c501dd159e2c5b36d0578dafa26666d91c1d99fe
hanmar88 <hanmar88@naver.com>
2016-10-22 17:35:19 +0900

    modified

--- naver01.cpp
+++ naver01.cpp
@@ -11,17 +11,19 @@ int main() {
 	int max=0;
 	map<string, int>::iterator it;
 
-	while(getline(cin, name)) {
+	while (getline(cin, name)) {
 		it = m.find(name);
-		if(it == m.end()) {
+		if (it == m.end()) {
 			m[name] = 1;
 		} else {
 			m[name]++;
 		}
-		if (max < m[name]) max = m[name];
+		if (max < m[name]) {
+			max = m[name];
+		}
 	}
 
-	for(it = m.begin();it != m.end();++it) {
+	for (it = m.begin();it != m.end();++it) {
 		if (it->second == max) {
 			cout << it->first << endl;
 		}
